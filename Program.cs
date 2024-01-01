01fbfa7446e0b6ce78d84fe8d79b2cd0ee946473
thiagocantero <thiagocantero@gmail.com>
2024-01-01 08:19:58 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Server=localhost;Database=dotnet;User=<usuario do banco>;Password=<senha do banco>;";
+        MySqlConnection connection = new MySqlConnection(connectionString);
+
+        try
+        {
+            connection.Open();
+            Console.WriteLine("Conectado ao MariaDB!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro: {ex.Message}");
+        }
+        finally
+        {
+            connection.Close();
+        }
+    }
+}
